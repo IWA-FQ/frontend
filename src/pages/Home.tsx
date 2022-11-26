313bd19db3513e0083df8e252f3f24e5d3366cf1
Quentin Desbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-11-26 18:34:53 +0100

    init frontend with react ts, tailwind and mui and begin login page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="font-bold">
+            <h1  className="font-bold">Homepage</h1>
+        </div>
+    );
+};
+
+export default Home;
