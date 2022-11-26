0f25e2af0921f4b4664bc53f39446a8a9c8f90f4
Quentin Desbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-11-26 18:34:53 +0100

    init frontend with react ts, tailwind and mui and begin login page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'primary':'#029BDD',
+      'secondary':'#296E8D',
+      'tertiary':'#F2F2F0',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
