a69ef19ceba0be29a37de49688783e0ec5e885e4
Quentin Desbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-11-26 19:52:42 +0100

    add login and home templates

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Recover from "./pages/Recover";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
             <Route path="/" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/recover" element={<Recover/>} />
             <Route path="/home" element={<Home/>} />
         </Routes>
       </BrowserRouter>
