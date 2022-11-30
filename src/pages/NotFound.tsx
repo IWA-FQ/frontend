c1b63ab0dabb3ceabe06815236804ce400dd2318
Quentin Desbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-11-30 15:09:03 +0100

    update offerList and userList, create offerDetails and Account

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NavigationBar from "../components/NavigationBar";
+import Footer from "../components/Footer";
+
+const NotFound = () => {
+    return (
+        <div>
+            <NavigationBar/>
+            <div className="flex flex-col justify-center items-center min-h-screen text-secondary gap-y-6">
+                <h1 className="lg:text-5xl md:text-4xl">Sorry, but this page doesn't exist</h1>
+                <p className="text-lg">Erreur http 404</p>
+            </div>
+            <Footer/>
+        </div>
+
+    );
+};
+
+export default NotFound;
