47872bceccce17452e3ab63dffe5c54917e21fab
Alexandre <alexandre.fernique@etu.umontpellier.fr>
2022-12-02 09:58:01 +0100

    fix env variable

--- src/services/http.service.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const get = () => {
-    return axios.get("http://jsonplaceholder.typicode.com/todos")
-}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,5 +4,5 @@ import {getTocken} from "./authentication.service";
 export const getAllUsers = () : Promise<AxiosResponse> => {
     const token = getTocken();
     const tokenToGive: string = token ? "Bearer " + token : "";
-    return axios.get("http://localhost:8080/api" + "/user/", {headers: {Authorization: tokenToGive}})
+    return axios.get(process.env.REACT_APP_API_URL + "/user/", {headers: {Authorization: tokenToGive}})
 }
